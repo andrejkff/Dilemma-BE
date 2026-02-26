02227a75057fa17be829751b3dd5a973b878105f
andrej4544940 <kolarovski.andrej@gmail.com>
2026-02-26 15:39:29 +0100

    Getting fields

--- src/services/level_up.ts
+++ src/services/level_up.ts
@@ -2,7 +2,7 @@ import { query } from '../db.js';
 
 async function getLevelUpNextStatus(level_up_id: string): Promise<any[] | false> {
   const { rows: nextStatusRows } = await query(
-    'SELECT * FROM level_up_next_status WHERE level_up_id = $1',
+    'SELECT status_id, text, remove_status_id, hide_for_status_id FROM level_up_next_status WHERE level_up_id = $1',
     [level_up_id]
   );
   if (!nextStatusRows.length) return false;
