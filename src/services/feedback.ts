160775319af6070161db6a74a9a5cec292a7b825
andrej4544940 <kolarovski.andrej@gmail.com>
2026-03-02 18:59:50 +0100

    Service cleanup

--- src/services/feedback.ts
+++ src/services/feedback.ts
@@ -1,11 +1,10 @@
 import { query } from '../db.js';
 
 async function saveSession(sessionId: string, gameId: number): Promise<void> {
-  const result = await query(
+  await query(
     `INSERT INTO game_sessions VALUES($1, $2)`,
     [sessionId, gameId]
   );
-  console.log(result)
 };
 
 export default {
