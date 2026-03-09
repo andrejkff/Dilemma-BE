707f3289c1c11325ab6a083058fa5af921b8f9f5
andrej4544940 <kolarovski.andrej@gmail.com>
2026-03-09 14:56:21 +0100

    Outcomes include point limits adjust

--- src/services/data/outcomes.ts
+++ src/services/data/outcomes.ts
@@ -24,15 +24,28 @@ async function getOutcomeExcludedStatuses(outcome_id: string): Promise<any[]> {
   return rows;
 };
 
+async function getOutcomePointLimitsAdjust(outcome_id: string): Promise<any[]> {
+  const { rows } = await query(
+    `SELECT o.min_value_increment, o.max_value_increment, p.slot_key
+    FROM outcome_point_limits_adjust o
+    JOIN point_slots p ON p.id = o.point_slot_id
+    WHERE o.outcome_id = $1`,
+    [outcome_id]
+  );
+  return rows;
+};
+
 async function renderOutcomeView(outcome_row: any, pointSlots: any[], statuses: Array<{ id: string}>): Promise<any> {
   const [
     nextPoints,
     nextStatuses,
     excludedStatuses,
+    pointAdjustRows,
   ] = await Promise.all([
     getOutcomeNextPoints(outcome_row['id']),
     getOutcomeNextStatuses(outcome_row['id']),
     getOutcomeExcludedStatuses(outcome_row['id']),
+    getOutcomePointLimitsAdjust(outcome_row['id']),
   ]);
   const is_for_statuses = statuses.map(s => s.id).filter(id => !excludedStatuses.find(es => es.status_id === id));
   const next_points: any = {};
@@ -44,12 +57,20 @@ async function renderOutcomeView(outcome_row: any, pointSlots: any[], statuses: 
       do_not_affect_dependent_slots: np.do_not_affect_dependent_slots || false,
     };
   });
+  const point_limits_adjust: any = {};
+  pointAdjustRows.forEach(par => {
+    point_limits_adjust[par.slot_key] = {
+      min_value_increment: par.min_value_increment,
+      max_value_increment: par.max_value_increment
+    }
+  });
   const ret: any = {
     ...outcome_row,
     next_points,
     next_statuses,
     is_for_statuses,
     required_documents: [],
+    point_limits_adjust,
   };
 
   return ret;
